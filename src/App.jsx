c3c36a239faca15eac27cc945470077cb7d9ab8e
Alina-ps <alinaserg2014@gmail.com>
2024-11-25 20:27:20 +0100

    basic route structure in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { lazy } from 'react';
+import Header from './components/Header/Header.jsx';
+
+const Home = lazy(() => import('./pages/HomePage/HomePage.jsx'));
+const Catalog = lazy(() => import('./pages/CatalogPage/CatalogPage.jsx'));
+const Details = lazy(() => import('./pages/DetailsPage/DetailsPage.jsx'));
+const Features = lazy(() => import('./components/Features/Features.jsx'));
+const Reviews = lazy(() => import('./components/Reviews/Reviews.jsx'));
+const NotFound = lazy(() => import('./pages/NotFoundPage/NotFoundPage.jsx'));
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/catalog" element={<Catalog />}></Route>
+        <Route path="/catalog/:id" element={<Details />}>
+          <Route path="features" element={<Features />}></Route>
+          <Route path="reviews" element={<Reviews />}></Route>
+        </Route>
+
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
