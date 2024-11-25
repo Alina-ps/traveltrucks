b5373a8cf542eeed070bf7dc24da8ba47ee5f969
Alina-ps <alinaserg2014@gmail.com>
2024-11-25 23:06:58 +0100

    home page
    1. Added elements based on Figma's template.
    2. Styled elements with CSS.
    3. Implemented navigation for Home and Catalog pages.
    4. Added navigation from the button to the Catalog page.
    5. Connected and applied the font to the project.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,27 @@
+import { Link, NavLink } from 'react-router-dom';
+import s from './Header.module.css';
+import clsx from 'clsx';
+
 const Header = () => {
-  return <div>Header</div>;
+  const buildLinkClass = ({ isActive }) => {
+    return clsx(s.link, isActive && s.active);
+  };
+
+  return (
+    <div className={s.header}>
+      <Link to="/" className={s.logo}>
+        Travel<span className={s.logoSpan}>Trucks</span>
+      </Link>
+      <div className={s.nav}>
+        <NavLink className={buildLinkClass} to="/">
+          Home
+        </NavLink>
+        <NavLink className={buildLinkClass} to="/catalog">
+          Catalog
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+import s from './HomePage.module.css';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className={s.container}>
+      <h1 className={s.title}>Campers of your dreams</h1>
+      <p className={s.paragraph}>
+        You can find everything you want in our catalog
+      </p>
+      <Link to="/catalog">
+        <button className={s.btn}>View Now</button>
+      </Link>
+    </div>
+  );
 };
 
 export default HomePage;
