3e5ff0a44a79d20b08f7d20ba3e29c16d0fb92ca
Alina-ps <alinaserg2014@gmail.com>
2024-12-01 18:10:31 +0100

    home page upd

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,13 +4,15 @@ import s from './HomePage.module.css';
 const HomePage = () => {
   return (
     <div className={s.container}>
-      <h1 className={s.title}>Campers of your dreams</h1>
-      <p className={s.paragraph}>
-        You can find everything you want in our catalog
-      </p>
-      <Link to="/catalog">
-        <button className={s.btn}>View Now</button>
-      </Link>
+      <div className={s.containerText}>
+        <h1 className={s.title}>Campers of your dreams</h1>
+        <p className={s.paragraph}>
+          You can find everything you want in our catalog
+        </p>
+        <Link to="/catalog">
+          <button className={s.btn}>View Now</button>
+        </Link>
+      </div>
     </div>
   );
 };
